1d932753744b2957958870b613a8e4d50601c789
coder lol <njohnson092008@gmail.com>
2025-11-19 19:35:37 -0500

    meow

--- transmitter.lua
+++ transmitter.lua
@@ -1,18 +1,31 @@
 local modem = peripheral.find("modem") or error("No Modem Found!", 0)
 modem.open(321)
 
+local function shorten(n)
+    if n >= 1e15 then
+        return string.format("%.1fP", n / 1e15)
+    elseif n >= 1e12 then
+        return string.format("%.1fT", n / 1e12)
+    elseif n >= 1e9 then
+        return string.format("%.1fB", n / 1e9)
+    elseif n >= 1e6 then
+        return string.format("%.1fM", n / 1e6)
+    elseif n >= 1e3 then
+        return string.format("%.1fK", n / 1e3)
+    else
+        return tostring(n)
+    end
+end
+
 local core = peripheral.find("draconic_rf_storage")
 local energy = peripheral.wrap("back")
-local capacity = energy.getMaxEnergyStored()
-
-if core then
-    local coreenergy = capacity
-    print(capacity)
-end
 
 local sleeptime = 2.5
 
 while true do
-    modem.transmit (321, 321, coreenergy())
+    local capacity = energy.getMaxEnergyStored()
+    local formatted = shorten(capacity)
+
+    modem.transmit (321, 321, formatted)
     sleep(sleeptime)
 end
